cdd82e754a4b4616438a0a5ea01452b227278ae4
aowens35 <107882438+aowens35@users.noreply.github.com>
2023-03-01 10:48:30 -0500

    Add files via upload

---
+++ blog/app.js
@@ -0,0 +1,97 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
+
+// express app
+const app = express();
+
+// connect to mongodb
+const dBURI = 'mongodb+srv://ajowens314:aliceinspacefightingzombies@nodetuts.oovzcik.mongodb.net/nodetuts?retryWrites=true&w=majority';
+mongoose.set('strictQuery', true);
+mongoose.connect(dBURI, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then((result)=> app.listen(3000))
+    .catch((err)=> console.log(err));
+
+
+//register view engine
+app.set('view engine', 'ejs');
+
+
+
+//middleware and static files
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(morgan('dev'));
+
+
+
+
+
+app.get('/', (req, res) =>{
+
+    res.redirect('/blogs')
+
+}); 
+
+app.get('/about', (req, res) =>{
+   // res.send('<p>About Page</p>');
+   res.render('about', {title: 'About'})
+}); 
+
+app.get('/blogs', (req, res) =>{
+    Blog.find().sort({createdAt: -1})
+    .then((result) =>{
+        res.render('index', {title:'All blogs', blogs: result})
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+});
+
+
+app.get('/blogs/create', (req, res) =>{
+    res.render('create', {title: 'Create a new Blog'})
+});
+
+//post new blogs
+
+app.post('/blogs', (req, res)=>{
+    const blog = new Blog(req.body);
+    blog.save()
+        .then((result)=>{
+            res.redirect('/blogs')
+        })
+    .catch((err)=>{
+        console.log(err);
+    });
+
+});
+
+app.get('/blogs/:id', (req, res)=>{
+    const id = req.params.id;
+    Blog.findById(id)
+    .then((result) =>{
+        res.render('details', { blog: result, title:'Blog Details'})
+    })
+    .catch((err)=>{
+        console.log(err);
+    });
+});
+
+
+app.delete('/blogs/:id', (req, res)=>{
+    const id = req.params.id;
+
+    Blog.findByIdAndDelete(id)
+    .then(result => {
+        res.json({redirect: '/blogs'})
+    })
+    .catch(err => {
+        console.log(err)
+    })
+})
+app.use((req, res) => {
+    res.status(404).render('404', {title: '404'});
+});
+
